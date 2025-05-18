8f59ce1ea23cf11ba350ac7e5ada0bccaa334bbd
nisrinba <nisrinebakhouch1@gmail.com>
2025-05-18 22:09:09 +0100

    frontend  and structure of project

--- RGPD_Project/backend/upload/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DashboardStatsView, FileUploadView, AnalysisListView
-
-urlpatterns = [
-    path('dashboard/stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
-    path('upload/', FileUploadView.as_view(), name='file-upload'),
-    path('analyses/', AnalysisListView.as_view(), name='analysis-list'),
-]
